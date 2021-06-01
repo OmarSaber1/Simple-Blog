0e3c393db1af02b38d460a19dd08e456b440def9
OmarSaber-os <osaber218@gmail.com>
2021-06-02 00:57:49 +0200

    add Registeration route + validations

--- routes/Users.js
+++ routes/Users.js
@@ -1,11 +1,66 @@
 const express = require("express");
+const { body, validationResult } = require("express-validator");
 const router = express.Router();
 
 // import user Model
 const User = require("../Models/users");
 
-router.get("/", (req, res) => {
-  res.send(`User Router is running`);
+/// get all users
+router.get("/", async (req, res) => {
+  try {
+    const users = await User.find({});
+    res.status(200).json({ users });
+  } catch (err) {
+    console.error(err);
+    res.status(404).json({ err });
+  }
 });
 
+/// user Register
+router.post(
+  "/",
+  [
+    body("name", "Name is required").not().isEmpty(),
+    body("email", "Please include a valid email").isEmail(),
+    body(
+      "password",
+      "Please enter a password within 6 or more character"
+    ).isLength({ min: 6 }),
+  ],
+  async (req, res) => {
+    // error if not all required data sent
+
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({
+        errors: errors.array(),
+      });
+    }
+    try {
+      // destructing Body => name , email , password
+
+      const { name = "", email = "", password = "" } = req.body;
+
+      // get user by email
+
+      const userExist = await User.findOne({ email }).exec();
+
+      // check if user already exists!
+      console.log(`check user is ${userExist}`);
+
+      // Send error
+      if (userExist) {
+        return res.status(400).send({ error: `User Already Exists` });
+      }
+
+      // if not found create it
+
+      const users = await User.create({ name, password, email });
+      res.status(200).json({ users });
+    } catch (err) {
+      console.error(err);
+      res.status(404).json({ err });
+    }
+  }
+);
 module.exports = router;
