1c17c910feffcc06dad035275773041ba1652187
OmarSaber-os <osaber218@gmail.com>
2021-06-01 23:15:54 +0200

    add user Model

--- server.js
+++ server.js
@@ -1,13 +1,18 @@
 const express = require("express");
 const app = express();
+const router = express.Router();
 
 app.use(express.json());
 
-const PORT = process.env.PORT | 5000;
 
 app.get("/", (req, res) => {
   res.send(`We are running`);
 });
+
+router.use('api/users',userRouter)
+//////////////////////////////////////////////// PORT //////////////////////////////////
+const PORT = process.env.PORT | 5000;
+
 app.listen(PORT, () =>
   console.log(`App is running and listening on PORT ${PORT}`)
 );
