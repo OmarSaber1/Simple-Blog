6b7ebc4ba3e58e4ab538d831268485de16259721
OmarSaber-os <osaber218@gmail.com>
2021-06-02 01:37:17 +0200

    Add Authorization middleware

--- routes/Auth.js
+++ routes/Auth.js
@@ -1,9 +1,10 @@
 const express = require("express");
-
 const router = express.Router();
 
-router.get("/", (req, res) => {
-  res.send(`Auth route is running`);
+const auth = require("../middlewares/Auth");
+
+router.post("/", auth, (req, res) => {
+  res.send(`You are authorized`);
 });
 
 module.exports = router;
