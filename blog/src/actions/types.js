097a122a99c054f3b2d208e0fb541deb4f5fcb7c
OmarSaber-os <osaber218@gmail.com>
2021-06-04 08:30:21 +0200

    add profile actions

--- blog/src/actions/types.js
+++ blog/src/actions/types.js
@@ -1,3 +1,4 @@
+//auth
 export const REGISTER_SUCCESS = "REGISTER_SUCCESS";
 export const REGISTER_FAIL = "REGISTER_FAIL";
 export const USER_LOADED = "USER_LOADED";
@@ -5,4 +6,9 @@ export const AUTH_ERROR = "USER_LOADED";
 export const LOGIN_SUCCESS = "LOGIN_SUCCESS";
 export const LOGIN_FAIL = "LOGIN_FAIL";
 export const LOGOUT = "LOGOUT";
+
+// profile
+export const GET_PROFILES = "GET_PROFILES";
+export const GET_PROFILE = "GET_PROFILE";
+export const PROFILE_ERR = "PROFILE_ERR";
 export const CLEAR_PROFILE = "CLEAR_PROFILE";
