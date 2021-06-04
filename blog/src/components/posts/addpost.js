77b05d99a4e28abd5e767c4bdfc9dfbee2bca43f
OmarSaber-os <osaber218@gmail.com>
2021-06-04 11:34:14 +0200

    add addPost component

---
+++ blog/src/components/posts/addpost.js
@@ -0,0 +1,57 @@
+import React from "react";
+import { Formik, Form, Field } from "formik";
+import * as Yup from "yup";
+import { addPost } from "../../actions/posts";
+import { connect } from "react-redux";
+
+const DisplayingErrorMessagesSchema = Yup.object().shape({
+  text: Yup.string().required("Required!"),
+});
+
+const createPost = ({ addPost }) => {
+  return (
+    <>
+      <div
+        style={{ backgroundColor: "violet" }}
+        className="w-100 p-2 rounded-3"
+      >
+        Add a post...
+      </div>
+
+      <Formik
+        initialValues={{
+          text: "",
+        }}
+        validationSchema={DisplayingErrorMessagesSchema}
+        onSubmit={(values, { resetForm }) => {
+          addPost({ text: values.text });
+          resetForm({});
+        }}
+      >
+        {({ errors, touched }) => (
+          <Form>
+            {console.log(errors)}
+            <Field
+              className="p-3 w-100 h-25"
+              name="text"
+              cols="30"
+              rows="5"
+              placeholder="Create a post..."
+            ></Field>
+            {touched.text && errors.text && (
+              <div style={{ color: "red" }}>
+                {" "}
+                <small>*{errors.text}</small>{" "}
+              </div>
+            )}
+            <button className="btn btn-primary w-100 mt-4 mb-4 " type="submit">
+              Submit!
+            </button>
+          </Form>
+        )}
+      </Formik>
+    </>
+  );
+};
+
+export default connect(null, { addPost })(createPost);
