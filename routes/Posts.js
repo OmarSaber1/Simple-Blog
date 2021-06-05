b030cb8f9d317a9281df3b4c4a94e5859603bb1a
OmarSaber-os <osaber218@gmail.com>
2021-06-05 11:45:14 +0200

    update route

--- routes/Posts.js
+++ routes/Posts.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const auth = require("../middlewares/Auth");
 const { body, validationResult } = require("express-validator");
 const User = require("../Models/users");
-const Post = require("../models/posts");
+const Post = require("../models/Posts");
 
 //////////////////// GET ALL POSTs //////////////////////
 
