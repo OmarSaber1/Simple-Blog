aa7432ccc6d494bda87f163f5497aeaadc287560
OmarSaber-os <osaber218@gmail.com>
2021-06-05 11:55:53 +0200

    improve server.js file

--- server.js
+++ server.js
@@ -31,7 +31,7 @@ if (process.env.NODE_ENV === "production") {
   });
 }
 //////////////////////////////////////////////// PORT //////////////////////////////////
-const PORT = process.env.PORT | 5000;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () =>
   console.log(`App is running and listening on PORT ${PORT}`)
